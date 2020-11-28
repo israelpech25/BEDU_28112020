5ceaeebf66a959dbe6cb692b0b2354b591a206d0
israelpech25 <israelhernandezpech@gmail.com>
2020-11-28 12:23:14 -0600

    bot de imagenes

--- 06_funcion_filter.py
+++ 06_funcion_filter.py
@@ -1,16 +1,13 @@
-#Usando la función filter de python
+# Usando la funcion filter de python
 
-#funciones 
+# Funciones
 def numero_par(numero):
     if numero % 2 == 0:
-        return True 
+        return True
 
-#Ejecucion
-
+# Ejecucion
 numeros = [3, 148, 74, 71, 4, 83, 95, 20, 61, 10, 69, 67, 23, 164, 97, 67, 144, 200, 38, 90, 200, 162, 6, 180, 65, 71, 90, 182, 16, 132, 182, 108, 90, 196, 48, 2, 158, 88, 39, 39, 54, 80, 89, 3, 90, 170, 88, 71, 142, 45, 81, 194, 36, 39, 30, 33, 38, 44, 134, 43, 12, 52, 170, 162, 192, 83, 18, 176, 120, 28, 86, 188, 51, 11, 96, 13, 198, 34, 66, 23, 200, 62, 194, 91, 51, 26, 152, 186, 86, 38, 46, 66, 83, 66, 40, 2, 20, 12, 91, 53]
 
 numeros_pares = list(filter(numero_par, numeros))
-
 numeros_pares.sort(reverse=True)
-
-print(numeros_pares)
+# print(numeros_pares)
---
+++ 07_github_api.py
@@ -0,0 +1,18 @@
+import requests
+
+#constants
+BASE_URL = 'https://api.github.com/'
+
+#functions
+def get_github_user(username):
+    url = f'{BASE_URL}users/{username}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+username = input('Give me a Github username:\t')
+
+user = get_github_user(username)
+
+print(user)
