8181a9c2c762ab8bebe19190aa6b4902ed172a95
israelpech25 <israelhernandezpech@gmail.com>
2020-11-28 11:06:31 -0600

    Tareas

---
+++ 04_pozoleria_descuento.py
@@ -0,0 +1,7 @@
+# Aplicar conocimientos de uso de la funcion map en python
+
+# Agregar un descuento aleatorio de entre 5, 10 y 15 porciento a todos los precios
+# Imprimir menu sin descuento
+# imprimir menu con descuento
+
+precios_en_menu = [1975, 1825, 275, 1500, 850, 675, 1175, 1600, 2175, 2175, 625, 1150, 1175, 350, 1125, 1250, 1875, 1275, 825, 1575, 300, 1275, 875, 1650, 875]
