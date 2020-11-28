9ccbd694a1f70b29f0060bc0843d87d4dc6c64eb
israelpech25 <israelhernandezpech@gmail.com>
2020-11-28 13:00:50 -0600

    bot de imagenes

--- 07_github_api.py
+++ 07_github_api.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 #constants
 BASE_URL = 'https://api.github.com/'
@@ -11,8 +12,18 @@ def get_github_user(username):
         return response.json()
     return None
 
+def download_github_user_avatar(avatar_url, username):
+    response = requests.get(avatar_url)
+    if response.status_code == 200:
+        #download a file from internet
+        response_content = response.content
+        filename = f'tmp/{username}.png'
+        with open(filename, 'wb') as image:
+            image.write(response_content)
+            return filename
+    return None
+
 username = input('Give me a Github username:\t')
-
 user = get_github_user(username)
-
-print(user)
+filename = download_github_user_avatar(user['avatar_url'], username)
+print(filename)
